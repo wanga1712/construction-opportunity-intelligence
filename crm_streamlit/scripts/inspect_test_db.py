4e37df9d9f072999d963d69f29c244087882f0f8
wangr <wangraf@yandex.ru>
2026-08-28 19:34:05 +0300

    debug: print detail for 900000700

--- crm_streamlit/scripts/inspect_test_db.py
+++ crm_streamlit/scripts/inspect_test_db.py
@@ -1,15 +1,21 @@
 from src.services.db_bootstrap import connect_databases
 
 def main():
-    _, _, crm_db, _ = connect_databases()
-    if crm_db:
-        res = crm_db.execute_query("""
-            SELECT table_name 
-            FROM information_schema.tables 
-            WHERE table_schema = 'public' 
-              AND table_name = 'crm_v3_autonomous_analysis_traces'
-        """)
-        print("RESULT:", res)
+    _, t, c, _ = connect_databases()
+    if c:
+        print("TRACES:")
+        traces = c.execute_query("SELECT id, attempt_count, consensus_state, research_completeness, document_set_hash, extracted_evidence_hash FROM crm_v3_autonomous_analysis_traces WHERE procurement_id = 900000700")
+        for tr in traces:
+            print(tr)
+    if t:
+        print("QUEUE:")
+        queue = t.execute_query("SELECT id, procurement_id, pipeline_generation, status FROM document_processing_queue WHERE procurement_id = 900000700")
+        for q in queue:
+            print(q)
+        print("FILES:")
+        files = t.execute_query("SELECT id, file_name, download_status, url, url_hash FROM document_files WHERE procurement_id = 900000700")
+        for f in files:
+            print(f)
 
 if __name__ == '__main__':
     main()
